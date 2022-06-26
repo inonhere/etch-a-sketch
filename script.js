40f4eee0b051daa3622b00274c4ba148fb1dd2b0
Inon <pokemonxdloquendo@gmail.com>
2022-06-26 11:56:01 -0400

    add 4x4 and 6x6

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+color = 'white'
+
 function rojo() {
     color = 'red';
 }
@@ -6,21 +8,50 @@ function azul() {
     color ='blue'
 }
 
-function crearGrid() {
+function crearGrid4() {
     for (let i = 0; i < 16; i++) {
-        const container = document.querySelector('.container');
+        p = document.querySelector('p');
+        p.classList.remove('container', 'container6');
+        p.classList.add('container');
+        const container = document.querySelector('#especialito');
         const grid = document.createElement('div');
         grid.classList.add('cuadrado');
         container.appendChild(grid);
         const elementos = document.getElementsByClassName('cuadrado');
     for (const elemento of elementos) {
         elemento.style.background = 'black';
-        const pruebita = document.getElementsByClassName('cuadrado');
-    for (const prueba of pruebita) {
-        prueba.addEventListener("mouseenter", function(event) {
+        const varios = document.getElementsByClassName('cuadrado');
+    for (const solo of varios) {
+        solo.addEventListener("mouseenter", function(event) {
         event.target.style.background = color;
     })
 }}}}
+
+function crearGrid6() {
+    for (let i = 0; i < 36; i++) {
+        p = document.querySelector('p');
+        p.classList.remove('container', 'container6');
+        p.classList.add('container6');
+        const grid = document.createElement('div');
+        const container = document.querySelector('#especialito');
+        grid.classList.add('cuadrado');
+        container.appendChild(grid);
+        const elementos = document.getElementsByClassName('cuadrado');
+    for (const elemento of elementos) {
+        elemento.style.background = 'black';
+        const varios = document.getElementsByClassName('cuadrado');
+    for (const solo of varios) {
+        solo.addEventListener("mouseenter", function(event) {
+        event.target.style.background = color;
+    })
+}}}}
+
+function borrarGrid() {
+    const contenedor = document.querySelector('#especialito');
+    while (contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild)
+    }
+    }
 
 /*
 for (let i = 0; i < 1; i++) {
