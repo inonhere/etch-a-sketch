b7a1f4695aa290deb3e39b1433261fc26836047d
Inon <pokemonxdloquendo@gmail.com>
2022-06-26 12:26:50 -0400

    centered grid, better grid creation code

--- script.js
+++ script.js
@@ -27,12 +27,12 @@ function crearGrid4() {
         contenedor.removeChild(contenedor.firstChild)}
     for (let i = 0; i < 16; i++) {
         p = document.querySelector('p');
-        p.classList.remove('container', 'container6', 'container8');
+        p.classList.remove('container', 'container6', 'container8', 'container16');
         p.classList.add('container');
         const container = document.querySelector('#especialito');
         const grid = document.createElement('div');
         grid.classList.add('cuadrado');
-        container.appendChild(grid);
+        container.appendChild(grid);}
         const elementos = document.getElementsByClassName('cuadrado');
     for (const elemento of elementos) {
         elemento.style.background = 'black';
@@ -41,7 +41,7 @@ function crearGrid4() {
         solo.addEventListener("mouseenter", function(event) {
         event.target.style.background = color;
     })
-}}}}
+}}}
 
 function crearGrid6() {
     const contenedor = document.querySelector('#especialito');
@@ -49,12 +49,12 @@ function crearGrid6() {
         contenedor.removeChild(contenedor.firstChild)}
     for (let i = 0; i < 36; i++) {
         p = document.querySelector('p');
-        p.classList.remove('container', 'container6', 'container8');
+        p.classList.remove('container', 'container6', 'container8', 'container16');
         p.classList.add('container6');
         const grid = document.createElement('div');
         const container = document.querySelector('#especialito');
         grid.classList.add('cuadrado');
-        container.appendChild(grid);
+        container.appendChild(grid);}
         const elementos = document.getElementsByClassName('cuadrado');
     for (const elemento of elementos) {
         elemento.style.background = 'black';
@@ -63,7 +63,51 @@ function crearGrid6() {
         solo.addEventListener("mouseenter", function(event) {
         event.target.style.background = color;
     })
-}}}}
+}}}
+
+function crearGrid8() {
+    const contenedor = document.querySelector('#especialito');
+    while (contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild)}
+    for (let i = 0; i < 84; i++) {
+        p = document.querySelector('p');
+        p.classList.remove('container', 'container6', 'container8', 'container16');
+        p.classList.add('container8');
+        const grid = document.createElement('div');
+        const container = document.querySelector('#especialito');
+        grid.classList.add('cuadrado');
+        container.appendChild(grid);}
+        const elementos = document.getElementsByClassName('cuadrado');
+    for (const elemento of elementos) {
+        elemento.style.background = 'black';
+        const varios = document.getElementsByClassName('cuadrado');
+    for (const solo of varios) {
+        solo.addEventListener("mouseenter", function(event) {
+        event.target.style.background = color;
+    })
+}}}
+
+function crearGrid16() {
+    const contenedor = document.querySelector('#especialito');
+    while (contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild)}
+    for (let i = 0; i < 256; i++) {
+        p = document.querySelector('p');
+        p.classList.remove('container', 'container6', 'container8', 'container16');
+        p.classList.add('container16');
+        const grid = document.createElement('div');
+        const container = document.querySelector('#especialito');
+        grid.classList.add('cuadrado');
+        container.appendChild(grid);}
+        const elementos = document.getElementsByClassName('cuadrado');
+    for (const elemento of elementos) {
+        elemento.style.background = 'black';
+        const varios = document.getElementsByClassName('cuadrado');
+    for (const solo of varios) {
+        solo.addEventListener("mouseenter", function(event) {
+        event.target.style.background = color;
+    })
+}}}
 
 
 function borrarGrid() {
