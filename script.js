e4fc0e57503c76a03e3d34aca84e94f448492f4d
Inon <pokemonxdloquendo@gmail.com>
2022-06-26 11:26:45 -0400

    add colors button

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
- for (let i = 0; i < 16; i++) {
+function rojo() {
+    color = 'red';
+}
+
+function azul() {
+    color ='blue'
+}
+
+function crearGrid() {
+    for (let i = 0; i < 16; i++) {
         const container = document.querySelector('.container');
         const grid = document.createElement('div');
         grid.classList.add('cuadrado');
         container.appendChild(grid);
-        }
+        const elementos = document.getElementsByClassName('cuadrado');
+    for (const elemento of elementos) {
+        elemento.style.background = 'black';
+        const pruebita = document.getElementsByClassName('cuadrado');
+    for (const prueba of pruebita) {
+        prueba.addEventListener("mouseenter", function(event) {
+        event.target.style.background = color;
+    })
+}}}}
+
+/*
+for (let i = 0; i < 1; i++) {
+    const container = document.querySelector('.container');
+    const grid = document.createElement('div');
+    grid.classList.add('cuadrado');
+    container.appendChild(grid);
+    }
 
 
 const pruebas = document.getElementsByClassName('cuadrado');
 for (const prueba of pruebas) {
-    prueba.style.background = 'green';
+prueba.style.background = 'green';
 }
 
 const pruebita = document.getElementsByClassName('cuadrado');
 for (const prueba of pruebita) {
-    prueba.addEventListener("mouseenter", function(event) {
-        event.target.style.background = 'black';
-    })
+prueba.addEventListener("mouseenter", function(event) {
+    event.target.style.background = 'black';
+})
 }
+
+*/
